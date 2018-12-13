297bedca32a127b08a5a3a45afc2ef3ec18497c3
derun <derun.va@gmail.com>
2018-12-13 20:27:58 +0200

    404 title

--- src/views/404.vue
+++ src/views/404.vue
@@ -3,3 +3,12 @@
     <h1>Page not found</h1>
   </div>
 </template>
+<script>
+  import { EventBus } from '../event-bus.js';
+  export default{
+    created : function(){
+      console.log('pricing')
+      EventBus.$emit('setHeading', {title: 'ERROR404 page not found!!!'});
+    }
+  }
+</script>
